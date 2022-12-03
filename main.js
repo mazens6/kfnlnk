fa5342520d0c08ee5dfc2fe914b1dd1ccca340fc
mazen <109769999+mazens6@users.noreply.github.com>
2022-12-03 21:38:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log(67766..toString());
+console.log((67766.55555).toFixed(2));
+console.log(Number.isInteger(100.5));
+console.log(Number.isInteger(100));
+console.log(parseInt (10000.55));
+console.log(parseFloat (1000.55));
+
